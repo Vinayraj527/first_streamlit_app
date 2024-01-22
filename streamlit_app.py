206bd7a54b46a2569921e3f73665132d684a5ca2
Vinayraj527 <157361318+Vinayraj527@users.noreply.github.com>
2024-01-22 15:50:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text(' 🥗 Kale Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞
-Avocado Toast')
+streamlit.text(' 🥑 🍞 Avocado Toast')
+
