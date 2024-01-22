fe82f42ba51d91741442e0389333c4ec7f7535f0
Vinayraj527 <157361318+Vinayraj527@users.noreply.github.com>
2024-01-22 15:59:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.text(' 🥗 Kale Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑 🍞 Avocado Toast')
 
-streamlit.title('My Mom's Healthy Diner')
+streamlit.title('My Moms Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text(' 🥗 Kale Spinach & Rocket Smoothie')
