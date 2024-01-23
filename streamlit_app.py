91e10b1bc860cea9e98760bb6fa56935cbcaac8b
Vinayraj527 <157361318+Vinayraj527@users.noreply.github.com>
2024-01-23 16:08:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,11 +30,13 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
-#---fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-#---streamlit.text(fruityvice_response.json())
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+#streamlit.text(fruityvice_response.json())
+
 
 # importing the  pandas pckg and normalize function to load it in table?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
