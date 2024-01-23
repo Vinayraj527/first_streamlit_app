acc42647e49434269663b86df9b02e0ab299de8b
Vinayraj527 <157361318+Vinayraj527@users.noreply.github.com>
2024-01-23 15:59:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # importing the  pandas pckg and normalize function to load it in table?
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
