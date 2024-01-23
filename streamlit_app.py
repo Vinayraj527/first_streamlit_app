61394ce725f03f9627717fc35e3623935946f013
Vinayraj527 <157361318+Vinayraj527@users.noreply.github.com>
2024-01-24 00:48:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,13 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-else:
-#streamlit.write('The user entered ', fruit_choice)
-#import requests
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-# importing the  pandas pckg and normalize function to load it in table?
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# normalizing the fruityvice table?
-  streamlit.dataframe(fruityvice_normalized)
+        streamlit.error("Please select a fruit to get information.")
+   else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
-  streamlit.error()
-
+    streamlit.error()
 streamlit.stop()
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
